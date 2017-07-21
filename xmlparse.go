316e0ecb9e4a5eb2dcc0c460b40572ba981d9f13
Durham <CDURHAM@arinc.com>
2017-07-21 15:31:17 -0500

    Identify intersections and output to file

--- xmlparse.go
+++ xmlparse.go
@@ -1,45 +1,48 @@
 package main
 
 import (
+	"bufio"
 	"encoding/xml"
 	"fmt"
 	"io"
 	"os"
 	"path/filepath"
+
+	"github.com/twmb/algoimpl/go/graph"
 )
 
-type ND struct {
+type nd struct {
 	XMLName xml.Name `xml:"nd"`
 	Ref     string   `xml:"ref,attr"`
 }
 
-type Tag struct {
+type tag struct {
 	XMLName xml.Name `xml:"tag"`
 	Kay     string   `xml:"k,attr"`
 	Vee     string   `xml:"v,attr"`
 }
 
-type XMLWay struct {
+type xmlway struct {
 	XMLName xml.Name `xml:"way"`
-	Refs    []ND     `xml:"nd"`
-	Tags    []Tag    `xml:"tag"`
+	Refs    []nd     `xml:"nd"`
+	Tags    []tag    `xml:"tag"`
 }
 
-type XMLNode struct {
+type xmlnode struct {
 	XMLName   xml.Name `xml:"node"`
 	ID        string   `xml:"id,attr"`
 	Latitude  float64  `xml:"lat,attr"`
 	Longitude float64  `xml:"lon,attr"`
 }
 
-type OSM struct {
+type osm struct {
 	XMLName xml.Name  `xml:"osm"`
-	Nodes   []XMLNode `xml:"node"`
-	Ways    []XMLWay  `xml:"way"`
+	Nodes   []xmlnode `xml:"node"`
+	Ways    []xmlway  `xml:"way"`
 }
 
-func ReadXML(reader io.Reader) ([]XMLWay, []XMLNode, error) {
-	var osm OSM
+func readXML(reader io.Reader) ([]xmlway, []xmlnode, error) {
+	var osm osm
 	if err := xml.NewDecoder(reader).Decode(&osm); err != nil {
 		return nil, nil, err
 	}
@@ -47,20 +50,25 @@ func ReadXML(reader io.Reader) ([]XMLWay, []XMLNode, error) {
 	return osm.Ways, osm.Nodes, nil
 }
 
-type NodeSlice []XMLNode
-
-func (slice NodeSlice) Len() int           { return len(slice) }
-func (slice NodeSlice) Less(i, j int) bool { return slice[i].ID > slice[j].ID }
-func (slice NodeSlice) Swap(i, j int)      { slice[i], slice[j] = slice[j], slice[i] }
-
-type IDCount struct {
+type idCount struct {
 	ID    []string
 	count []int
-	Roads []string
 }
 
-func FindID(id string, idSlice *IDCount) {
-	var idFound bool = false
+// We only want "ways" that have a named road otherwise it is an alley or private drive
+func wayIsARoad(way xmlway) bool {
+	var foundNamedRoad = false
+	for i := range way.Tags {
+		if way.Tags[i].Kay == "name" {
+			foundNamedRoad = true
+		}
+	}
+
+	return foundNamedRoad
+}
+
+func findIDAndAddToList(id string, idSlice *idCount) {
+	var idFound = false
 
 	for i := 0; i < len(idSlice.ID) && !idFound; i++ {
 		if idSlice.ID[i] == id {
@@ -73,6 +81,32 @@ func FindID(id string, idSlice *IDCount) {
 		idSlice.ID = append(idSlice.ID, id)
 		idSlice.count = append(idSlice.count, 1)
 	}
+}
+
+// IDs (locations) with a count less than 2 are not at an intersection and we don't care about them
+func removeIDThatHaveCountLessThanTwo(idSlice *idCount) {
+	for i := range idSlice.ID {
+		if idSlice.count[i] < 2 {
+			idSlice.ID = append(idSlice.ID[:i], idSlice.ID[i+1:]...)
+			idSlice.count = append(idSlice.count[:i], idSlice.count[i+1:]...)
+		}
+	}
+}
+
+func writeIntersectionsToFile(string fileName, idSlice idCount) error {
+	file, err := os.Create(fileName)
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	w := bufio.NewWriter(file)
+	for i := range idSlice.ID {
+		fmt.Fprintln(w, idSlice.ID[i])
+	}
+
+	return w.Flush()
 }
 
 func main() {
@@ -90,17 +124,22 @@ func main() {
 
 	defer file.Close()
 
-	xmlWay, xmlNodes, err := ReadXML(file)
+	ways, nodes, err := readXML(file)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	var listOfIDs IDCount
-	for i := range xmlWay {
-		for j := range xmlWay[i].Refs {
-			FindID(xmlWay[i].Refs[j].Ref, &listOfIDs)
+	var listOfIDs idCount
+	for i := range ways {
+		for j := range ways[i].Refs {
+			if wayIsARoad(ways[i]) {
+				findIDAndAddToList(ways[i].Refs[j].Ref, &listOfIDs)
+			}
 		}
 	}
+
+	removeIDThatHaveCountLessThanTwo(&listOfIDs)
+	writeIntersectionsToFile("listOfIntersections.txt", listOfIDs)
 
 	count := 0
 	for i := range listOfIDs.ID {
@@ -109,6 +148,10 @@ func main() {
 			fmt.Printf("ID: %s Count: %d\n", listOfIDs.ID[i], listOfIDs.count[i])
 		}
 	}
+
 	fmt.Printf("Number of intersections: %d\n", count)
-	fmt.Printf("Number of nodes: %d\n", len(xmlNodes))
+	fmt.Printf("Number of nodes: %d\n", len(nodes))
+
+	g := graph.New(graph.Undirected)
+	nodes := make(map[rune]graph.Node, 0)
 }
